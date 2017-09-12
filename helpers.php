5b0a354821bec5b257041d2da0d943a15fda3171
Maxwell Carvalho <maxwell@viewup.com.br>
2017-09-12 09:38:37 -0300

    update helpers

--- helpers.php
+++ helpers.php
@@ -18,8 +18,20 @@ function wpcg_get() {
  *
  * @param string $id
  * @param bool $default Setting Default(if not set, uses the defined default)
+ *
+ * @return string
  */
-function the_setting( $id = '', $default = false ) {
+function wpcg_get_setting( $id = null, $default = false ) {
+	return wpcg_get()->get_setting( $id, $default );
+}
+
+/**
+ * Output the setting using the defined function
+ *
+ * @param string $id
+ * @param bool $default Setting Default(if not set, uses the defined default)
+ */
+function wpcg_the_setting( $id = null, $default = false ) {
 	wpcg_get()->the_setting( $id, $default );
 }
 
@@ -28,6 +40,27 @@ function the_setting( $id = '', $default = false ) {
  *
  * @param string|array|null $id The setting ID, or array of settings IDs, or NULL if the current Setting
  */
-function setting_attributes( $id = null ) {
+function wpcg_the_setting_attribute( $id = null ) {
 	wpcg_get()->setting_attributes( $id );
 }
+
+
+// smaller alternatives
+
+if ( ! function_exists( 'get_setting' ) ):
+	function get_setting( $id = null, $default = false ) {
+		return wpcg_get_setting( $id, $default );
+	}
+endif;
+
+if ( ! function_exists( 'the_setting' ) ):
+	function the_setting( $id = null, $default = false ) {
+		wpcg_the_setting( $id, $default );
+	}
+endif;
+
+if ( ! function_exists( 'the_setting_attribute' ) ):
+	function the_setting_attribute( $id = null ) {
+		wpcg_the_setting_attribute( $id );
+	}
+endif;
